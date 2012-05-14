460026aa85f6abc47902c9fad48601fd7dc9339a
unknown <glauco@Hephaestus.(none)>
2012-05-14 07:58:53 -0700

    adding the latest version of devday website

---
+++ DevDay/Controllers/DivulgueController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DevDay.Controllers
+{
+    public class DivulgueController : Controller
+    {
+        //
+        // GET: /Divulgue/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ DevDay/Controllers/InscricoesController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DevDay.Controllers
+{
+    public class InscricoesController : Controller
+    {
+        //
+        // GET: /Inscricoes/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
