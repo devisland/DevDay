60f3999214642bc4a34c7f11608881de46255ef5
renatosaito <renato.saito@live.com>
2012-07-12 23:21:48 -0300

    improved ui and minor fixes

---
+++ DevDay/Content/js/divulgue.js
@@ -0,0 +1,3 @@
+﻿$('#shareontwitter').on('click', function () {
+    window.open('https://twitter.com/share?original_referer=http://devday.devisland.com&text=Developers%20Whats%20your%20profession?!!%20DevDay%202012%20eu%20vou!', 'twitter', 'WIDTH=800, HEIGHT=600');
+});
