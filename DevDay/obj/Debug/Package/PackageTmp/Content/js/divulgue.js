74a1113465f1ceadd0a8dd0ea7d3bbeef6a8298e
Gibran Silva <gibran.silva@gmail.com>
2012-07-21 13:38:01 -0300

    Remove Bin and Obj

--- DevDay/obj/Debug/Package/PackageTmp/Content/js/divulgue.js
+++
@@ -1,7 +0,0 @@
-﻿$('#shareontwitter').on('click', function () {
-    window.open('https://twitter.com/share?original_referer=http://devday.devisland.com&text=Developers%20Whats%20your%20profession?!!%20DevDay%202012%20eu%20vou!', 'twitter', 'WIDTH=800, HEIGHT=600');
-});
-
-$('#shareonfacebook').on('click', function () {
-    window.open('https://www.facebook.com/DevIsland', 'facebook', 'WIDTH=800, HEIGHT=600');
-});
--- DevDay/obj/Debug/Package/PackageTmp/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DevDay.MvcApplication" Language="C#" %>
