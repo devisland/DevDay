19f69d3da9366ceaaa04161839141e8840ac9994
Renato Saito <renato.saito@live.com>
2012-07-20 21:49:15 -0300

    image gallery, social info and event date added

---
+++ DevDay/Controllers/ContatoController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace DevDay.Controllers
+{
+    public class ContatoController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ DevDay/Controllers/GalleryController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace DevDay.Controllers
+{
+    public class GalleryController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
