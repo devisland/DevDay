5fe36a21dedefaa48f5754fde5064f47436c1b02
Gibran Silva <gibran.silva@gmail.com>
2012-09-01 19:48:34 -0300

    Enquete de Premios

---
+++ DevDay/Controllers/EnqueteController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DevDay.Controllers
+{
+    public class EnqueteController : Controller
+    {
+        //
+        // GET: /Enquete/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
